cd8da29cadbe58067f63a8dca7b5521109ac727a
Seer00 <1014616839@qq.com>
2020-07-23 14:03:21 +0800

    add NO.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 	修改 MIN 和 MAX 的宏定义，即可改变输出
 */
 
+/*
+	新增：输出带序号功能
+	2020-07-23
+*/
+
 #include <iostream>
 #define min 80 
 #define max 120 
@@ -13,8 +18,8 @@ int main()
 {
 	int i;
 	for (i = min; i < max; i++) {
-		cout << i << endl;
-		// printf("%d\n", i);
+		// 输出数据前带有从1开始递增的序号
+		cout << (i - min + 1) << ":" << i << endl;
 	}
 
 	system("pause");
